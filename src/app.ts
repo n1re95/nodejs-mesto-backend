4dac787cde297b3850e1ecf631c1e1dd649bf1e6
Ananyev Nikita <mgfile08@gmail.com>
2024-10-02 20:59:15 +0300

    router

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
 import express from 'express';
 import { connect as connectToDb } from './database';
 import { init as initEnv } from './environment';
-import { userRouter } from './routes';
+import { router } from './routes';
 
 const run = async () => {
   initEnv();
   await connectToDb();
   const app = express();
   app.use(express.json());
+  app.use(router);
   app.listen(process.env.PORT);
-  app.use('/user', userRouter);
 };
 
 run();
