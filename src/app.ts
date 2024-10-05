97c021bf75b10cda2375f04e857f1a288dbb23df
Ananyev Nikita <mgfile08@gmail.com>
2024-10-05 09:08:32 +0300

    auth

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,12 @@ import express from 'express';
 import { connect as connectToDb } from './database';
 import { init as initEnv } from './environment';
 import { router } from './routes';
-import { checkAuth } from './middleware';
 
 const run = async () => {
   initEnv();
   await connectToDb();
   const app = express();
   app.use(express.json());
-  app.use(checkAuth);
   app.use(router);
   app.listen(process.env.PORT);
 };
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { userRouter } from './user';
+import { checkAuth } from '../middleware';
 
 export const router = express.Router();
 
+router.use(checkAuth);
 router.use('/user', userRouter);
